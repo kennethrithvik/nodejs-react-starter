3331af155bd1b5a828ad2f9d16b81b2a02b3e03d
kennethrithvik <kennethrithvik@yahoo.in>
2017-11-24 14:37:15 +0530

    added page not found page

---
+++ client/src/components/common/NotFound.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+let divStyle={
+    fontSize:'80px',
+    fontWeight:'900px',
+    color:'#000000',
+    textAlign:'center',
+    paddingTop: '30vh',
+    paddingBottom: '30vh'
+
+};
+let NotFound = () => (
+    <div style={divStyle}>
+        404&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;PAGE NOT FOUND
+    </div>
+);
+
+export default NotFound;
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import NotFound from '../components/common/NotFound'
 import logo from './logo.svg';
 import './App.css';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -7,16 +9,20 @@ import RaisedButton from 'material-ui/RaisedButton';
 class App extends Component {
 
   render() {
+    let msg =()=> (<div><p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+                </p>
+                <RaisedButton label="Hello" /></div>);
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <RaisedButton label="Hello" />
+        <Switch>
+          <Route exact path="/" component={msg} />
+          <Route component={NotFound} />
+        </Switch>
       </div>
     );
   }
